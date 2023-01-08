9f128de5db0fe5583c9694786f6b5c89493019a5
NKDark <nkdark214@gmail.com>
2023-01-08 21:14:43 +0800

    update

--- src/main/kotlin/top/nkdark/gocq/handler/BotWebSocketHandler.kt
+++ src/main/kotlin/top/nkdark/gocq/handler/BotWebSocketHandler.kt
@@ -8,6 +8,7 @@ import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
 import top.nkdark.gocq.bot.ApiHandler
+import top.nkdark.gocq.bot.Bot
 import top.nkdark.gocq.bot.BotContainer
 import top.nkdark.gocq.bot.BotFactory
 import java.util.concurrent.ExecutorService
@@ -20,41 +21,37 @@ class BotWebSocketHandler(
     private val executor: ExecutorService,
     private val botContainer: BotContainer,
 ) : TextWebSocketHandler() {
+
     private val log = LoggerFactory.getLogger(BotWebSocketHandler::class.java)
+
     override fun afterConnectionEstablished(session: WebSocketSession) {
         val xSelfId = session.handshakeHeaders["x-self-id"]?.get(0)?.toLong()
         log.info("{} connected", xSelfId)
         xSelfId?.let { uin ->
             val bot = botFactory.createBot(uin, session)
-            botContainer.bots.put(uin, bot)
+            botContainer.bots[uin] = bot
         }
     }
 
     override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
         val xSelfId = session.handshakeHeaders["x-self-id"]?.get(0)?.toLong()
         log.info("{} disconnected", xSelfId)
-        xSelfId?.let {
-            botContainer.bots.remove(it)
-        }
+        botContainer.bots.remove(xSelfId)
     }
 
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
         val xSelfId = session.handshakeHeaders["x-self-id"]?.get(0)?.toLong()
         xSelfId?.let { uin ->
-            var bot = botContainer.bots[uin]
-            if (null == bot) {
-                bot = botFactory.createBot(uin, session)
-                botContainer.bots[uin] = bot
-            }
+            val bot: Bot = botContainer.bots[uin]
+                ?: botFactory.createBot(uin, session).also { botContainer.bots[uin] = it }
+
             bot.botSession = session
 
-            val recvJson = JSON.parseObject(message.payload)
-            if (recvJson.containsKey("echo")) {
-                apiHandler.onReceiveApiMessage(recvJson)
+            val payloadJson = JSON.parseObject(message.payload)
+            if (payloadJson.containsKey("echo")) {
+                apiHandler.onReceiveApiMessage(payloadJson)
             } else {
-                executor.execute {
-                    eventHandler.handle(bot, recvJson)
-                }
+                executor.execute { eventHandler.handle(bot, payloadJson) }
             }
         }
     }
